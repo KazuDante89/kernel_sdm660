8e290c795a872a2fe520ae7ba3a64c2ccabb5233
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-11-22 16:39:44 -0500

    Couple more tweaks 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660-bus.dtsi 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi 	modified:   arch/arm64/configs/lavender_defconfig 	modified:   drivers/cpufreq/cpufreq.c 	modified:   kernel/sched/cpufreq_schedutil.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2291,8 +2291,10 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	if (ret)
 		return ret;
 
-	if (cpumask_test_cpu(new_policy->cpu, cpu_lp_mask))
-		  cpufreq_verify_within_limits(new_policy, 633600, new_policy->max);
+		if (cpumask_test_cpu(new_policy->cpu, cpu_lp_mask))
+			cpufreq_verify_within_limits(new_policy, 633600, new_policy->max);
+		else
+			cpufreq_verify_within_limits(new_policy, 1113600, new_policy->max);
 
 	/* notification of the new policy */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
