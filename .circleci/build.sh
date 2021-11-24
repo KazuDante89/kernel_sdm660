531fba4137642cd22abb64c30f0d73fb30db6815
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-11-24 11:44:37 -0500

    Test non_burst_sync_event on boe-td4320 panel 	modified:   .circleci/build.sh 	modified:   arch/arm64/boot/dts/vendor/qcom/xiaomi/longcheer/common/display/dsi-panel-boe-td4320-1080p-video.dtsi 	modified:   arch/arm64/boot/dts/vendor/qcom/xiaomi/longcheer/common/longcheer-sdm660-mdss.dtsi 	modified:   arch/arm64/configs/vendor/lavender_defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -60,7 +60,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [4.19]-Impact_v.0.0.zip *
+    zip -r9 [4.19]-Impact_v.0.1.zip *
     cd ..
 }
 sendinfo
